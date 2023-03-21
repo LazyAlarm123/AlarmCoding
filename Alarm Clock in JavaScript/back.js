507b4b4081d744935ca5c667902f244ac2f44597
supbest <99116141+supbest@users.noreply.github.com>
2023-03-22 03:17:25 +0700

    Add files via upload

--- Alarm Clock in JavaScript/back.js
+++ Alarm Clock in JavaScript/back.js
@@ -9,3 +9,18 @@ router.post('/saveAlarm', async (req, res) => {
   console.log(Data)
 });
 module.exports=router;
+
+router.delete('/del/:_id', async (req,res) => {
+  try {
+    const check = await alarm.findOne({id : req.body.id})
+
+      await alarm.findOneAndRemove({id : req.body.id});
+      res.send("delete");}
+      catch (e) {
+        response.status(500).send({ message: e.message });}}
+   );
+router.get('/all', async (req,res) =>{
+  alarm.find().then((a)=>res.json(a)).catch((err)=>console.log(err));
+})
+
+
