8686da304079a0f74eab4dd984859c543c7b25ab
Rawitch Payakkawan <rawitch13@gmail.com>
2023-03-23 23:00:08 +0700

    first commit

---
+++ Alarm Clock in JavaScript/Backend/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const alarmtime = require('./routes/route');
+
+var dotenv = require('dotenv');
+dotenv.config();
+const database = process.env.MONGOLAB_URI;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(database, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB Connected');
+  })
+  .catch((err) => {
+    console.log('MongoDB not Connected' + err);
+  });
+const db = mongoose.connection;
+db.on('error', (err) => {
+  console.log('Connection Error: ' + err);
+});
+app.get('/', function (req, res) {
+  // #swagger.ignore = true
+  res.redirect('/');
+});
+app.use('/api', alarmtime);
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log('Server is running at port 4000');
+});
